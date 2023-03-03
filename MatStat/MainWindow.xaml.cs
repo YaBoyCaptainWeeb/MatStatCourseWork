5e60f64c06262aab18daa939d987e5bc6c6cfe2f
Владислав <cymbalov_va@office.osu.ru>
2023-03-03 21:55:02 +0500

    03.03.2023 21:54

--- MatStat/MainWindow.xaml.cs
+++ MatStat/MainWindow.xaml.cs
@@ -51,7 +51,7 @@ namespace MatStat
 
             }
         }
-        List<List<string>> grid = new List<List<string>>();
+        List<List<string>> grid = new List<List<string>>(); // Исходные данные
         List<MyTable> itemsSource = new List<MyTable>();
         Microsoft.Office.Interop.Excel.Application Excel = new Microsoft.Office.Interop.Excel.Application();
 
@@ -61,29 +61,37 @@ namespace MatStat
             // Excel Data Reader
             // Excel Data Reader.Dataset
             
-            InitializeComponent();
+            InitializeComponent();        
+        }
+
+        private void Load(object sender, RoutedEventArgs e) // Источник данных
+        {
             try
             {
                 ExcelPackage.LicenseContext = OfficeOpenXml.LicenseContext.NonCommercial;
                 ExcelPackage excelPackage = new ExcelPackage(@".\Матрица.xlsx");
                 ExcelWorksheet excelWorksheet = excelPackage.Workbook.Worksheets[0];
-               for (int i = 2; true; i++)
-               {
+
+                for (int i = 2; true; i++) // копируем таблицу из Excel для внутренней работы программы
+                {
                     if (excelWorksheet.Cells[i, 1].Value == null) break;
                     List<string> row = new List<string>();
                     for (int j = 1; true; j++)
                     {
-                        if (excelWorksheet.Cells[i,j].Value == null) break;
+                        if (excelWorksheet.Cells[i, j].Value == null) break;
                         row.Add(excelWorksheet.Cells[i, j].Value.ToString());
                     }
                     grid.Add(row);
-               }
-               foreach(List<string> row in grid) // делаем копию в itemSource для загрузки данных в нужном формате для DataGrid
-               {
+                }
+
+                foreach (List<string> row in grid) // делаем копию в itemSource для загрузки данных в нужном формате для DataGrid
+                {
                     itemsSource.Add(new MyTable(row[0], Convert.ToDouble(row[1]), Convert.ToDouble(row[2]), Convert.ToDouble(row[3]),
                         Convert.ToDouble(row[4]), Convert.ToDouble(row[5]), Convert.ToDouble(row[6]), Convert.ToDouble(row[7]), Convert.ToDouble(row[8])));
-               }
-            } 
+                }
+
+
+            }
             catch (Exception)
             {
                 MessageBox.Show("Таблица не найдена/не может быть открыта.");
@@ -94,12 +102,97 @@ namespace MatStat
                     process.Kill();
                 }
                 Application.Current.Shutdown();
-            }            
+            }
+            dataGrid.ItemsSource = itemsSource;
+        }
+        private void Load1(object sender, RoutedEventArgs e) // Выборки, нормированная таблица(НЕ ДОДЕЛАНО ЕЩЕ ВЛАДИК ДОДЕЛОЙ???)
+                                                             // Нормирование есть, степень доверия осталась, не ебу как делоть
+        {
+            List<double> col = new List<double>();
+            List<MyTable> itemsSource = new List<MyTable>();
+            List<MyTable> metricsSource = new List<MyTable>();
+            List<List<string>> normedGrid = new List<List<string>>();
+
+            for(int i = 0; i != grid.Count; i++) // копия grid для внутренней работы функции
+            {
+                List<string> row = new List<string>();
+                for(int j = 0; j != grid[i].Count; j++)
+                {
+                    row.Add(grid[i][j]);
+                }
+                normedGrid.Add(row);
+            }
+            
+
+            for (int cols = 1; cols != 9; cols++) // нормирование
+            {
+                for (int rows = 0; rows != grid.Count; rows++)
+                {
+                    col.Add(Convert.ToDouble(grid[rows][cols]));
+                }
+                double colMax = col.Max();
+                double colMin = col.Min();
+                double Xnorm = 0;
+                for (int rows1 = 0; rows1 != normedGrid.Count; rows1++)
+                {
+                    Xnorm = (col[rows1] - colMin) / (colMax - colMin);
+                    normedGrid[rows1][cols] = Convert.ToString(Math.Round(Xnorm,2));
+                }
+            }
+
+            foreach(List<string> row in normedGrid)
+            {
+                itemsSource.Add(new MyTable(row[0], Convert.ToDouble(row[1]), Convert.ToDouble(row[2]), Convert.ToDouble(row[3]),
+                        Convert.ToDouble(row[4]), Convert.ToDouble(row[5]), Convert.ToDouble(row[6]), Convert.ToDouble(row[7]), Convert.ToDouble(row[8])));
+            }
+            NormGrid.ItemsSource = itemsSource;
+
+            List<string> array1 = new List<string>();   // среднее арифметическое
+            array1.Add("Среднее арифметическое");
+            for (int cols = 1; cols != 9; cols++) 
+            {
+                double srAriphmetic = 0;
+                for (int rows = 0; rows != normedGrid.Count; rows++)
+                {
+                    srAriphmetic += Convert.ToDouble(normedGrid[rows][cols]);
+                }
+                srAriphmetic /= normedGrid.Count;
+                array1.Add(Math.Round(srAriphmetic,2).ToString());
+            }
+            normedGrid.Clear();
+            normedGrid.Add(array1);
+            foreach (List<string> row in normedGrid)
+            {
+                metricsSource.Add(new MyTable(row[0], Convert.ToDouble(row[1]), Convert.ToDouble(row[2]), Convert.ToDouble(row[3]),
+                        Convert.ToDouble(row[4]), Convert.ToDouble(row[5]), Convert.ToDouble(row[6]), Convert.ToDouble(row[7]), Convert.ToDouble(row[8])));
+            }
+            Metrics.ItemsSource = metricsSource;
+
         }
 
-        private void Load(object sender, RoutedEventArgs e)
+        private void Load2(object sender, RoutedEventArgs e)
         {
-           dataGrid.ItemsSource = itemsSource;
+            List<MyTable> metricsSource = new List<MyTable>();
+
+            NormGrid.ItemsSource = itemsSource;
+
+            List<string> row = new List<string>();   // среднее арифметическое
+            row.Add("Среднее арифметическое");
+            for (int cols = 1; cols != 9; cols++)
+            {
+                double srAriphmetic = 0;
+                for (int rows = 0; rows != grid.Count; rows++)
+                {
+                    srAriphmetic += Convert.ToDouble(grid[rows][cols]);
+                }
+                srAriphmetic /= grid.Count;
+                row.Add(Math.Round(srAriphmetic, 2).ToString());
+            }
+
+            
+                metricsSource.Add(new MyTable(row[0], Convert.ToDouble(row[1]), Convert.ToDouble(row[2]), Convert.ToDouble(row[3]),
+                        Convert.ToDouble(row[4]), Convert.ToDouble(row[5]), Convert.ToDouble(row[6]), Convert.ToDouble(row[7]), Convert.ToDouble(row[8])));
+            Metrics.ItemsSource = metricsSource;
         }
 
         private void AppExit(object sender, CancelEventArgs e)
