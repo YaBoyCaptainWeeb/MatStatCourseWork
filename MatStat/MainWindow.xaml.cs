7ac0a00ac2d4186189303abca134574cf45637a0
Владислав <cymbalov_va@office.osu.ru>
2023-03-06 19:28:26 +0500

    06.03.2023

--- MatStat/MainWindow.xaml.cs
+++ MatStat/MainWindow.xaml.cs
@@ -61,7 +61,7 @@ namespace MatStat
             // Excel Data Reader
             // Excel Data Reader.Dataset
             
-            InitializeComponent();        
+            InitializeComponent();
         }
 
         private void Load(object sender, RoutedEventArgs e) // Источник данных
@@ -72,6 +72,7 @@ namespace MatStat
                 ExcelPackage excelPackage = new ExcelPackage(@".\Матрица.xlsx");
                 ExcelWorksheet excelWorksheet = excelPackage.Workbook.Worksheets[0];
 
+                itemsSource.Clear();
                 for (int i = 2; true; i++) // копируем таблицу из Excel для внутренней работы программы
                 {
                     if (excelWorksheet.Cells[i, 1].Value == null) break;
@@ -105,6 +106,7 @@ namespace MatStat
             }
             dataGrid.ItemsSource = itemsSource;
         }
+        #region Нормированные таблицы
         private void Load1(object sender, RoutedEventArgs e) // Выборки, нормированная таблица(НЕ ДОДЕЛАНО ЕЩЕ ВЛАДИК ДОДЕЛОЙ???)
                                                              // Нормирование есть, степень доверия осталась, не ебу как делоть
         {
@@ -112,6 +114,7 @@ namespace MatStat
             List<MyTable> itemsSource = new List<MyTable>();
             List<MyTable> metricsSource = new List<MyTable>();
             List<List<string>> normedGrid = new List<List<string>>();
+            List<List<string>> metricsGrid = new List<List<string>>();
 
             for(int i = 0; i != grid.Count; i++) // копия grid для внутренней работы функции
             {
@@ -146,7 +149,7 @@ namespace MatStat
                         Convert.ToDouble(row[4]), Convert.ToDouble(row[5]), Convert.ToDouble(row[6]), Convert.ToDouble(row[7]), Convert.ToDouble(row[8])));
             }
             NormGrid.ItemsSource = itemsSource;
-
+            ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
             List<string> array1 = new List<string>();   // среднее арифметическое
             array1.Add("Среднее арифметическое");
             for (int cols = 1; cols != 9; cols++) 
@@ -159,25 +162,89 @@ namespace MatStat
                 srAriphmetic /= normedGrid.Count;
                 array1.Add(Math.Round(srAriphmetic,2).ToString());
             }
-            normedGrid.Clear();
-            normedGrid.Add(array1);
-            foreach (List<string> row in normedGrid)
+            metricsGrid.Add(array1);
+            //////////////////////////////////////////////////////
+            List<string> array21 = new List<string>(); // Мода
+            array21.Add("Мода");
+            for (int cols = 1; cols != 9; cols++)
+            {
+                List<string> array2 = new List<string>();
+                for (int rows = 0; rows != normedGrid.Count; rows++)
+                {
+                    array2.Add(normedGrid[rows][cols]);
+                }
+                array21.Add(Moda(array2));
+            }
+            metricsGrid.Add(array21);
+            /////////////////////////////////////////////////////
+            List<string> array3 = new List<string>(); // Медиана
+            array3.Add("Медиана");
+            for (int cols = 1; cols != 9; cols++)
+            {
+                List<double> median = new List<double>();
+                for (int rows = 0; rows != normedGrid.Count; rows++)
+                {
+                    median.Add(Convert.ToDouble(normedGrid[rows][cols]));
+                }
+                array3.Add(Median(median));
+            }
+            metricsGrid.Add(array3);
+            ////////////////////////////////////////////////////////
+            List<string> array4 = new List<string>(); // Дисперсия
+            List<double> array41 = new List<double>();
+            array4.Add("Дисперсия");
+            for (int cols = 1; cols != 9; cols++)
+            {
+                array41.Clear();
+                double srAriphmetic = 0;
+                for (int rows = 0; rows != normedGrid.Count; rows++)
+                {
+                    srAriphmetic += Convert.ToDouble(normedGrid[rows][cols]);
+                }
+                srAriphmetic /= normedGrid.Count;
+                srAriphmetic = Math.Round(srAriphmetic, 2);
+                for (int rows = 0; rows != normedGrid.Count; rows++)
+                {
+                    array41.Add(Math.Pow((Convert.ToDouble(normedGrid[rows][cols]) - srAriphmetic), 2));
+                }
+                array4.Add(Convert.ToString(Math.Round(array41.Sum() / (normedGrid.Count - 1),2)));
+            }
+            metricsGrid.Add(array4);
+            ////////////////////////////////////////////////////////////
+            foreach (List<string> row in metricsGrid) // создание массива объектов MyTable после всех расчетов, должно быть в самом конце
             {
                 metricsSource.Add(new MyTable(row[0], Convert.ToDouble(row[1]), Convert.ToDouble(row[2]), Convert.ToDouble(row[3]),
                         Convert.ToDouble(row[4]), Convert.ToDouble(row[5]), Convert.ToDouble(row[6]), Convert.ToDouble(row[7]), Convert.ToDouble(row[8])));
             }
             Metrics.ItemsSource = metricsSource;
-
         }
-
-        private void Load2(object sender, RoutedEventArgs e)
+        #endregion
+        #region Не нормированные таблицы
+        private void Load2(object sender, RoutedEventArgs e) // не нормированная таблица
         {
+            List<MyTable> itemsSource = new List<MyTable>();
             List<MyTable> metricsSource = new List<MyTable>();
+            List<List<string>> table = new List<List<string>>();
+
+            for (int i = 0; i != grid.Count; i++) // копия grid для внутренней работы функции
+            {
+                List<string> row = new List<string>();
+                for (int j = 0; j != grid[i].Count; j++)
+                {
+                    row.Add(grid[i][j]);
+                }
+                table.Add(row);
+            }
+            foreach (List<string> row in table)
+            {
+                itemsSource.Add(new MyTable(row[0], Convert.ToDouble(row[1]), Convert.ToDouble(row[2]), Convert.ToDouble(row[3]),
+                        Convert.ToDouble(row[4]), Convert.ToDouble(row[5]), Convert.ToDouble(row[6]), Convert.ToDouble(row[7]), Convert.ToDouble(row[8])));
+            }
 
             NormGrid.ItemsSource = itemsSource;
 
-            List<string> row = new List<string>();   // среднее арифметическое
-            row.Add("Среднее арифметическое");
+            List<string> str = new List<string>();   // среднее арифметическое еще дорабатывать
+            str.Add("Среднее арифметическое");
             for (int cols = 1; cols != 9; cols++)
             {
                 double srAriphmetic = 0;
@@ -186,13 +253,35 @@ namespace MatStat
                     srAriphmetic += Convert.ToDouble(grid[rows][cols]);
                 }
                 srAriphmetic /= grid.Count;
-                row.Add(Math.Round(srAriphmetic, 2).ToString());
+                str.Add(Math.Round(srAriphmetic, 2).ToString());
             }
 
             
-                metricsSource.Add(new MyTable(row[0], Convert.ToDouble(row[1]), Convert.ToDouble(row[2]), Convert.ToDouble(row[3]),
-                        Convert.ToDouble(row[4]), Convert.ToDouble(row[5]), Convert.ToDouble(row[6]), Convert.ToDouble(row[7]), Convert.ToDouble(row[8])));
+                metricsSource.Add(new MyTable(str[0], Convert.ToDouble(str[1]), Convert.ToDouble(str[2]), Convert.ToDouble(str[3]),
+                        Convert.ToDouble(str[4]), Convert.ToDouble(str[5]), Convert.ToDouble(str[6]), Convert.ToDouble(str[7]), Convert.ToDouble(str[8])));
             Metrics.ItemsSource = metricsSource;
+        }
+        #endregion
+
+        private string Median(List<double> arr)
+        {
+            int numCount = arr.Count;
+            int halfindex = arr.Count / 2;
+            var sortedItems = arr.OrderBy(x => x);
+            double median;
+            if ((numCount%2) == 0)
+            {
+                median = ((sortedItems.ElementAt(halfindex) + sortedItems.ElementAt((halfindex - 1))) / 2);
+            } else
+            {
+                median = sortedItems.ElementAt(halfindex);
+            }
+            return Convert.ToString(median);
+        }
+
+        private string Moda(List<string> arr)
+        {
+            return arr.GroupBy(x => x).OrderByDescending(x => x.Count()).ThenBy(x => x.Key).Select(x => x.Key).First();
         }
 
         private void AppExit(object sender, CancelEventArgs e)
